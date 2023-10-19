0d82f1b57f8eda91c623950812effd1d5ed36e95
CarlosTarriasDiazWeb <carlos.tarrias.diaz.web@gmail.com>
2023-10-19 15:56:24 +0200

    User registration completed

--- games-swap-frontend/src/components/Login.vue
+++ games-swap-frontend/src/components/Login.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { type Ref, ref } from 'vue';
 import { VueFinalModal } from 'vue-final-modal';
 
 
@@ -6,21 +7,46 @@ const emit = defineEmits<{
   (e: 'confirm'): void,
   (e: 'cancel'): void
 }>()
+
+
+interface LoginType {
+  email: string,
+  password: string,
+}
+
+const formData: Ref<LoginType> = ref({
+  email: "",
+  password: "",
+})
+
+const sendData = () => {
+  fetch("http://localhost:8080/users/login", {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      email: formData.value.email,
+      password: formData.value.password
+    })
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+}
+
 </script>
 
 <template>
-  <VueFinalModal
-    class="confirm-modal"
-    content-class="confirm-modal-content"
-    overlay-transition="vfm-fade"
-    content-transition="vfm-fade"
-  >
-  <form @submit="(e) => e.preventDefault()">
-        <h1>INICIO SESIÓN</h1>
-        <input type="email" name="email" id="email" placeholder="Correo">
-        <input type="password" name="password" id="password" placeholder="Contraseña">
-        <button @click="emit('confirm')">ENTRAR</button>
-        <button class="register-btn" @click="emit('cancel')">¿No tienes cuenta? Regístrate</button>
+  <VueFinalModal class="confirm-modal" content-class="confirm-modal-content" overlay-transition="vfm-fade"
+    content-transition="vfm-fade">
+    <form @submit.prevent="sendData">
+      <h1>INICIO SESIÓN</h1>
+      <input type="email" name="email" id="email" placeholder="Correo">
+      <input type="password" name="password" id="password" placeholder="Contraseña">
+      <button @click="emit('confirm')">ENTRAR</button>
+      <button class="register-btn" @click="emit('cancel')">¿No tienes cuenta? Regístrate</button>
     </form>
   </VueFinalModal>
 </template>
@@ -31,61 +57,66 @@ const emit = defineEmits<{
   justify-content: center;
   align-items: center;
 }
+
 .confirm-modal-content {
-    display: flex;
-    flex-direction: column;
-    border-radius: 27px;
-    border: 1px solid var(--Color-3, #000);
-    background: var(--Color, #F6F6F6);
-    padding: 2rem;
-    width: 507px;
-    height: 438px;
-   
+  display: flex;
+  flex-direction: column;
+  border-radius: 27px;
+  border: 1px solid var(--Color-3, #000);
+  background: var(--Color, #F6F6F6);
+  padding: 2rem;
+  width: 507px;
+  height: 438px;
+
 }
+
 .dark .confirm-modal-content {
   background: #000;
 }
 
-h1{
-    color: #9F87F5;
-    text-align: center;
-    font-family: Roboto;
-    font-size: 30px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-}
-form {
-    display: flex;
-    flex-direction: column;
-}
-form input {
-    border-radius: 29px;
-    border: 3px solid #9F87F5;
-    background: #F6F6F6;
-    padding: 1rem;
-    margin-bottom: 50px;
+h1 {
+  color: #9F87F5;
+  text-align: center;
+  font-family: Roboto;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
 }
 
-form input #placeholder{
-    color:#D3D3D3;
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+form input {
+  border-radius: 29px;
+  border: 3px solid #9F87F5;
+  background: #F6F6F6;
+  padding: 1rem;
+  margin-bottom: 50px;
+}
+
+form input #placeholder {
+  color: #D3D3D3;
 }
 
 button {
-    background-color: #9F87F5;
-    color: white;
-    width: 70%;
-    margin: 0 auto;
-}
-button.register-btn {
-    margin-top: 1rem;
-    background-color: transparent;
-    color: #a2a2a2;
-    text-decoration: underline;
-    border: none;
-}
-button.register-btn:hover {
-    color: #000;
+  background-color: #9F87F5;
+  color: white;
+  width: 70%;
+  margin: 0 auto;
 }
 
+button.register-btn {
+  margin-top: 1rem;
+  background-color: transparent;
+  color: #a2a2a2;
+  text-decoration: underline;
+  border: none;
+}
+
+button.register-btn:hover {
+  color: #000;
+}
 </style>
--- games-swap-frontend/src/components/Register.vue
+++ games-swap-frontend/src/components/Register.vue
@@ -1,33 +1,66 @@
 <script setup lang="ts">
+import { ref, type Ref } from 'vue';
 import { VueFinalModal } from 'vue-final-modal';
 
 
 const emit = defineEmits<{
   (e: 'confirm'): void
 }>()
+
+interface RegisterType {
+  username: string,
+  email: string,
+  password: string,
+  password2: string,
+  checkbox: boolean
+}
+
+const formData: Ref<RegisterType> = ref({
+  username: "",
+  email: "",
+  password: "",
+  password2: "",
+  checkbox: false
+})
+
+const sendData = () => {
+  fetch("http://localhost:8080/users/register", {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      username: formData.value.username,
+      email: formData.value.email,
+      password: formData.value.password
+    })
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+}
+
 </script>
 
 <template>
-  <VueFinalModal
-    class="confirm-modal"
-    content-class="confirm-modal-content"
-    overlay-transition="vfm-fade"
-    content-transition="vfm-fade"
-  >
-  <form>
-        <h1>DATE DE ALTA</h1>
-       
-        <input type="name" name="name" id="name" placeholder="Nombre">
-        <input type="email" name="email" id="email" placeholder="Correo">
-        <input type="password" name="password" id="password" placeholder="Contraseña">
-        <input type="password2" name="password2" id="password2" placeholder="Repite contraseña">
-       
-        <div class="terms">
-            <input type="checkbox" name="conditions" id="conditions-text">
-        <label for="conditions" id="conditions">He leído y acepto las <u>Condiciones de uso</u> y la <u>Política de privacidad de GameSwap</u>.
-Ver +info sobre protección de datos</label>
-        </div>
-        <button>REGISTRARSE </button>
+  <VueFinalModal class="confirm-modal" content-class="confirm-modal-content" overlay-transition="vfm-fade"
+    content-transition="vfm-fade">
+    <form @submit.prevent="sendData()">
+      <h1>DATE DE ALTA</h1>
+
+      <input v-model="formData.username" type="name" name="name" id="name" placeholder="Nombre">
+      <input v-model="formData.email" type="email" name="email" id="email" placeholder="Correo">
+      <input v-model="formData.password" type="password" name="password" id="password" placeholder="Contraseña">
+      <input v-model="formData.password2" type="password" name="password2" id="password2" placeholder="Repite contraseña">
+
+      <div class="terms">
+        <input v-model="formData.checkbox" type="checkbox" name="conditions" id="conditions-text">
+        <label for="conditions" id="conditions">He leído y acepto las <u>Condiciones de uso</u> y la <u>Política de
+            privacidad de GameSwap</u>.
+          Ver +info sobre protección de datos</label>
+      </div>
+      <button type="submit">REGISTRARSE </button>
     </form>
   </VueFinalModal>
 </template>
@@ -38,54 +71,58 @@ Ver +info sobre protección de datos</label>
   justify-content: center;
   align-items: center;
 }
+
 .confirm-modal-content {
-    display: flex;
-    flex-direction: column;
-    border-radius: 27px;
-    border: 1px solid var(--Color-3, #000);
-    background: var(--Color, #F6F6F6);
-    padding: 2rem;
-   height: auto;
+  display: flex;
+  flex-direction: column;
+  border-radius: 27px;
+  border: 1px solid var(--Color-3, #000);
+  background: var(--Color, #F6F6F6);
+  padding: 2rem;
+  height: auto;
 }
+
 .dark .confirm-modal-content {
   background: #000;
 }
 
-h1{
-    color: #9F87F5;
-    text-align: center;
-    font-family: Roboto;
-    font-size: 1.5rem;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-}
-form {
-    display: flex;
-    flex-direction: column;
-}
-form input {
-    border-radius: 29px;
-    border: 3px solid #9F87F5;
-    background: #F6F6F6;
-    padding: 0.75rem;
-    margin-bottom: 2rem;
+h1 {
+  color: #9F87F5;
+  text-align: center;
+  font-family: Roboto;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
 }
 
-form input #placeholder{
-    color:#D3D3D3;
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+form input {
+  border-radius: 29px;
+  border: 3px solid #9F87F5;
+  background: #F6F6F6;
+  padding: 0.75rem;
+  margin-bottom: 2rem;
+}
+
+form input #placeholder {
+  color: #D3D3D3;
 }
 
 button {
-    background-color: #9F87F5;
-    color: white;
-    margin: 0 auto;
-    width: 70%;
-}
-.terms {
-    display: flex;
-    gap: 1rem;
-    margin-bottom: 2rem;
+  background-color: #9F87F5;
+  color: white;
+  margin: 0 auto;
+  width: 70%;
 }
 
+.terms {
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 2rem;
+}
 </style>
