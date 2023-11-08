acdeed2319c5d837f7aba6871de6600bdc5eb09a
CarlosTarriasDiazWeb <carlos.tarrias.diaz.web@gmail.com>
2023-11-08 22:58:28 +0100

    Criteria and genre filters implemented in category view

--- games-swap-frontend/src/views/AdsCategoryView.vue
+++ games-swap-frontend/src/views/AdsCategoryView.vue
@@ -9,16 +9,26 @@
       Cargando...
     </div> -->
     <div class="product-grid">
+      <select @change="updateCriteriaFilter" v-model="criteria">
+        <option value="Todos">Todos</option>
+        <option value="A-Z">A-Z</option>
+        <option value="Z-A" selected>Z-A</option>
+        <option value="priceDesc">Precio (menor a mayor)</option>
+        <option value="priceAsc">Precio (mayor a menor)</option>
+      </select>
       <!-- Genres in separate component. -->
-      <div class="genre-box" v-for="(genre, index) in genres" :key="genre.genre_id">
+      <div class="genre-box" v-for="(genre, index) in  genres " :key="genre.genre_id">
         <form @submit.prevent='updateGenreFilter(genre.genre_name, genre.genre_id)'>
-          <input type="submit" :value="genre.genre_name">
+          <input :class="{ 'input-active': genreFilter === genre.genre_id }" type="submit" :value="genre.genre_name">
         </form>
       </div>
-      <form @submit.prevent='updateGenreFilter("test", 2)'>
-        <input type="submit" value="Test">
-      </form>
-      <PostCard v-for="product in products" :key="product.post_id" :product="product"></PostCard>
+      <div class="genre-box">
+        <form @submit.prevent='genreFilter = -1'>
+          <input type="submit" value="Restaurar">
+        </form>
+      </div>
+
+      <PostCard v-for=" product  in  filteredProducts " :key="product.post_id" :product="product"></PostCard>
     </div>
     <Footer></Footer>
   </div>
@@ -44,14 +54,60 @@ onMounted(() => {
 })
 
 //const platformsStore = usePlatformsStore();
-const genreFilter: Ref<string | number> = ref(0);
+const genreFilter: Ref<number> = ref(-1);
 const platforms: Ref<Platform[]> = ref([]);
 const genres: Ref<Genre[]> = ref([]);
 const route = useRoute();
 const categoryId = ref(route.params.id);
 const products = ref<Array<Product>>([]);
 const isLoading = ref(true);
-//let filteredProducts = computed<Product[]>(() => console.log("test"))
+const criteria: Ref<string> = ref("");
+
+const filteredProducts = computed(() => {
+  if (genreFilter.value === -1 && criteria.value === "") {
+    return products.value;
+  }
+  else if (genreFilter.value !== -1 && criteria.value === "") {
+    return products.value.filter((product: Product) => product.genre_id === genreFilter.value);
+  }
+  else if (genreFilter.value === -1 && criteria.value !== "") {
+
+    const criteriaFilteredProducts = [...products.value];
+
+    if (criteria.value === "A-Z") {
+      return criteriaFilteredProducts.sort((a: Product, b: Product) => a.post_title.localeCompare(b.post_title));
+    }
+    else if (criteria.value === "Z-A") {
+      return criteriaFilteredProducts.sort((a: Product, b: Product) => b.post_title.localeCompare(a.post_title));
+    }
+    else if (criteria.value === "priceDesc") {
+      return criteriaFilteredProducts.sort((a: Product, b: Product) => a.post_price - b.post_price);
+    }
+    else if (criteria.value === "priceAsc") {
+      return criteriaFilteredProducts.sort((a: Product, b: Product) => b.post_price - a.post_price);
+    }
+  }
+  else if (genreFilter.value !== -1 && criteria.value !== "") {
+    const criteriaFilteredProducts = [...products.value];
+
+    if (criteria.value === "A-Z") {
+      criteriaFilteredProducts.sort((a: Product, b: Product) => a.post_title.localeCompare(b.post_title));
+    }
+    else if (criteria.value === "Z-A") {
+      criteriaFilteredProducts.sort((a: Product, b: Product) => b.post_title.localeCompare(a.post_title));
+    }
+    else if (criteria.value === "priceDesc") {
+      criteriaFilteredProducts.sort((a: Product, b: Product) => b.post_price - a.post_price);
+    }
+    else if (criteria.value === "priceAsc") {
+      criteriaFilteredProducts.sort((a: Product, b: Product) => a.post_price - b.post_price);
+    }
+    return criteriaFilteredProducts.filter((product: Product) => product.genre_id === genreFilter.value);
+  }
+
+}
+
+);
 
 //Filters
 // function applyFilters(): Product[] {
@@ -68,6 +124,10 @@ function updateGenreFilter(genre_name: string, id: number) {
   genreFilter.value = id;
   console.log(genreFilter.value);
   //applyFilters();
+}
+
+function updateCriteriaFilter() {
+
 }
 
 const fetchPlatforms = async (): Promise<{ message: string, categories: Platform[] } | undefined> => {
@@ -136,6 +196,11 @@ span {
 
 span {
   width: 100%;
+}
+
+.input-active {
+  background-color: #9f87f5;
+  ;
 }
 </style>
   
