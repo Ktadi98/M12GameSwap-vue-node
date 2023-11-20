4dc9eb6d710ecdbc8a3894116bfa33c54b59d429
carol b <carolina.blanco.7e6@itb.cat>
2023-11-20 16:13:00 +0100

    reset password component

--- games-swap-frontend/src/App.vue
+++ games-swap-frontend/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ModalsContainer } from 'vue-final-modal';
 import { RouterView } from 'vue-router';
-import ControlPanel from './views/ControlPanel.vue';
 
 </script>
 
